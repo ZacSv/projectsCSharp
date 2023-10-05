bc7b196b272723da5c650976358f93583223a1a3
Isac Vieira <isacvieira2009@hotmail.com>
2023-10-05 20:09:54 -0300

    Initialize project

---
+++ calculadora/Calculadora/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+   
+        static bool verificaValor(int arg)
+        {
+           if(arg < 0){
+
+                return false;
+            }
+            return true;
+        }
+        static void somaValores(int arg1, int arg2)
+        {
+           int valorTotal = arg1 + arg2;
+            bool resultado = verificaValor(valorTotal);
+            if(resultado == false)
+            {
+                Console.WriteLine("O resultado da operação retornou um negativo, tente novamente...");
+                
+            }
+            else
+            {
+                Console.WriteLine(valorTotal);
+            }
+            
+        }
+        static void subtraiValores(int arg1, int arg2)
+        {
+            int valorTotal = arg1 - arg2;
+            bool resultado = verificaValor(valorTotal);
+            if (resultado == false)
+            {
+                Console.WriteLine("O resultado da operação retornou um negativo, tente novamente...");
+
+            }
+            else
+            {
+                Console.WriteLine(valorTotal);
+            }
+        }
+        static void multiplicaValores(int arg1, int arg2)
+        {
+            int valorTotal = arg1 * arg2;
+            bool resultado = verificaValor(valorTotal);
+            if (resultado == false)
+            {
+                Console.WriteLine("O resultado da operação retornou um negativo, tente novamente...");
+
+            }
+            else
+            {
+                Console.WriteLine(valorTotal);
+            }
+        }
+        static void divideValores(int arg1, int arg2)
+        {
+            int valorTotal = arg1 / arg2;
+            bool resultado = verificaValor(valorTotal);
+            if (resultado == false)
+            {
+                Console.WriteLine("O resultado da operação retornou um negativo, tente novamente...");
+
+            }
+            else
+            {
+                Console.WriteLine(valorTotal);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Digite o segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine(
+                "Selecione a opção que deseja realizar utilizando os números do menu:\n" +
+                "1 - Soma\n" +
+                "2 - Subtração\n" +
+                "3 - Multiplicação\n" +
+                "4 - Divisão\n" +
+                "0 - Sair");
+            int opcaoUsuario = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(
+                    "Selecione a opção que deseja realizar utilizando os números do menu:\n" +
+                    "1 - Soma\n" +
+                    "2 - Subtração\n" +
+                    "3 - Multiplicação\n" +
+                    "4 - Divisão\n" +
+                    "0 - Sair");
+                opcaoUsuario = int.Parse(Console.ReadLine());
+
+                switch (opcaoUsuario)
+                {
+                    case 1:
+                        somaValores(numero1, numero2);
+                        break;
+                    case 2:
+                        subtraiValores(numero1, numero2);
+                        break;
+                    case 3:
+                        multiplicaValores(numero1, numero2);
+                        break;
+                    case 4:
+                        divideValores(numero1, numero2);
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Desculpe, não reconheci sua operação, tente novamente");
+                        break;
+                };
+
+            } while (opcaoUsuario != 0);
+
+
+            Console.WriteLine("Press any key to close the window...");
+            Console.ReadKey();
+        }
+    }
+}
