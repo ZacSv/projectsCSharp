30e1938edf2dfdc630638bbb43acedf77ea310b1
Isac Vieira <isacvieira2009@hotmail.com>
2023-11-06 16:20:58 -0300

    criando logica de funcionarios

--- POO-Conceitos/Banco-POO/Program.cs
+++ POO-Conceitos/Banco-POO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banco_POO.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace Banco_POO
     {
         static void Main(string[] args)
         {
+            Funcionario Isac = new Funcionario(ECargos.Diretoria);
+            Funcionario Fabiana = new Funcionario(ECargos.Gerencia);
+            Funcionario Juscelino = new Funcionario(ECargos.Monitoria);
+            Funcionario Jaqueline = new Funcionario(ECargos.Operacao);
+
+            Isac.Salario = 5000;
+            Fabiana.Salario = 5000;
+            Juscelino.Salario = 5000;
+            Jaqueline.Salario = 5000;
+
+            Console.WriteLine(Isac.GetBonificacao());
+            Console.WriteLine(Fabiana.GetBonificacao());
+            Console.WriteLine(Juscelino.GetBonificacao());
+            Console.WriteLine(Jaqueline.GetBonificacao());
+
+            Console.ReadKey();
         }
     }
 }
